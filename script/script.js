3d3bc6fc162572959103b5b1b5e40ebb95b6687b
maurizio-marotta <mauriziomarotta@live.it>
2023-03-25 19:20:09 +0100

    [soluzione]

---
+++ script/script.js
@@ -0,0 +1,14 @@
+
+//soluzione corretta
+for(let i = 1; i < 101; i++){
+  if(!(i % 15)){
+    console.log('fizzbuzz');
+  }else if(!(i % 3)){
+    console.log('fizz');
+  }else if(!(i % 5)){
+    console.log('buzz');
+  }else{
+    console.log(i);
+  }
+}
+
